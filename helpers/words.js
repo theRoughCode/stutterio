90b1b2d533d173ee2fe3425879669ee563ac9149
Amr Abouelkhair <abouelkhair.305@gmail.com>
2017-11-04 04:41:44 -0400

    added a function to get the first syllable from a word using words api

--- helpers/words.js
+++ helpers/words.js
@@ -4,26 +4,44 @@ function getDefaultText(callback) {
   callback(text);
 }
 
+var unirest = require('unirest');
+
 /*
- * Consumes a user id and his sample entry and finds every occurance of a stutter in his 
- * sample, then adds the stutter causing syllabal to the user's list of stuttering 
- * syllabals.
+ * Consumes a user id and his sample entry and finds every occurance of a stutter in his
+ * sample, then adds the stutter causing syllable to the user's list of stuttering
+ * syllables.
  */
-function findStuterSyllabals(text, user){
-  // go through the text word by word
-  // if word is a stuter
-  // then add its first syllabal to the user's list of stutter syllabals
+function findStuttersyllables(words, user){
+	for(var word in words){
+		//find the first syllable of the word using the words api
+		//add the first syllable to the user list of syllables
+	}
 }
 
 /*
- * Consumes a user id and a piece of text and returns the text after highlighting each word 
- * that starts with one of the user's stuttering syllabals.
+ * Consumes in a word and returns the first syllable of the word using words api
+ */
+function firstsyllable(word){
+	var request = "https://wordsapiv1.p.mashape.com/words/" + word
+	unirest.get(request)
+  .header("X-Mashape-Key", "uzoBeHZ3TkmshlrZiPD8eWLQQZF9p1sRnxJjsnMzgqCMQB9Xnh")
+  .header("Accept", "application/json")
+  .end(function (result) {
+    var parsedWord = JSON.parse(result.body);
+    var syllables = JSON.parse(parsedWord.syllables).list;
+    return syllables[0];
+	});
+}
+/*
+ * Consumes a user id and a piece of text and returns the text after highlighting each word
+ * that starts with one of the user's stuttering syllables.
  */
 function listOfStutterWords(text, user){
    // go through the text word by word
-   // if words starts with one of the user's stuttering syllabals 
-   // highlight word by adding tags before and after it 
+   // if words starts with one of the user's stuttering syllables
+   // highlight word by adding tags before and after it
 }
 module.exports = {
+  findStuttersyllables
   getDefaultText
 }
