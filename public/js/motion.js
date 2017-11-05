4c2e8f77429e954089ded698a84074aec9093b6c
pratyushpal <pratyush98pal@gmail.com>
2017-11-04 23:27:38 -0400

    Fixed the gestures

--- public/js/motion.js
+++ public/js/motion.js
@@ -35,19 +35,26 @@
   var controller = Leap.loop({enableGestures: true}, function(frame){
   if(frame.valid && frame.gestures.length > 0){
     frame.gestures.forEach(function(gesture){
-        switch (gesture.type){
-          case "circle":
-              console.log("Circle Gesture");
-              break;
-          case "keyTap":
-              console.log("Key Tap Gesture");
-              break;
-          case "screenTap":
-              console.log("Screen Tap Gesture");
-              break;
-          case "swipe":
-              console.log("Swipe Gesture");
-              break;
+       (gesture.type === "swipe"){
+            //Classify swipe as either horizontal or vertical
+            var isHorizontal = Math.abs(gesture.direction[0]) > Math.abs(gesture.direction[1]);
+            //Classify as right-left or up-down
+            if(isHorizontal){
+              if(gesture.direction[0] > 0){
+                swipeDirection = "right";
+              } else {
+                swipeDirection = "left";
+              }
+            } else { //vertical
+              if(gesture.direction[1] > 0){
+                swipeDirection = "up";
+              } else {
+                swipeDirection = "down";
+              }
+            }
+          console.log(swipeDirection);
+          console.log("Swipe Gesture");
+                break;
         }
     });
   }
